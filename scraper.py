751272bf4466b7f1640f23811fd6061874839678
Matthew P. Bagin <matthew.bagin@gmail.com>
2020-09-12 13:22:35 -0400

    added cookies handling but not working

--- scraper.py
+++ scraper.py
@@ -1,18 +1,29 @@
+import requests
 from requests_html import HTML, HTMLSession
 import smtplib
 import HashPassword as Hash
 
 
-def scrape_page(url,):
+def scrape_page(url):
 
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
     }
+
+    cookies = requests.cookies.RequestsCookieJar()
+    cookies.set("store", "7115", domain=".homedepot.ca")
+
     session = HTMLSession()
-    r = session.get(url, headers=headers,)
-    # page_html = r.text
+
+    session.cookies = cookies
+
+    r = session.get(url, headers=headers)
+    page_html = r.text
 
     # print(r.text.encode("utf-8"))
+
+    for c in r.cookies:
+        print(c)
 
     with open("page_raw_html.html", "rb+") as html_file:
 
@@ -21,12 +32,17 @@ def scrape_page(url,):
         source = html_file.read()
         html = HTML(html=source)
 
-        # for li in html.find(".simple li", _encoding="utf-8"):
-        #     print(li.text.encode("utf-8"))
+        for a in html.find(".acl-display--flex"):
 
-    hd_prods = {
-        "MicroPro Sienna 1 x 6 x 6'": "micropro-sienna-1-x-6-x-6-treated-wood-fence-board/1000790632",
-    }
+            for b in a.find(".acl-mr--xx-small"):
+
+                for c in b.find(".ng-star-inserted"):
+                    print(c.text.encode("utf-8"))
+
+            for d in a.find(".acl-link--weak"):
+
+                for e in d.find(".ng-star-inserted"):
+                    print(e.text.encode("utf-8"))
 
     return
 
@@ -59,7 +75,11 @@ if __name__ == "__main__":
     user = "matthew.bagin@gmail.com"
     pw = Hash.decrypt(key, cipher_text).decode("utf-8")
 
-    print(f"user name is {user}")
+    scrape_page(
+        "https://www.homedepot.ca/product/micropro-sienna-1-x-6-x-6-treated-wood-fence-board/1000790632"
+    )
+
+    # print(f"user name is {user}")
     # print(f"password is {pw}")
 
     # send_mail(user, pw)
