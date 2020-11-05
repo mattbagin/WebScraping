26b96746541c5b534bd21d69503fe0cbf8d72b5a
Matthew P. Bagin <matthew.bagin@gmail.com>
2020-11-05 14:54:06 -0500

    sends email when product is in stock

--- scraper.py
+++ scraper.py
@@ -15,23 +15,36 @@ def scrape_page(url):
 
     html = HTML(html=page_html)
 
-    with open("page_raw_html.html", "rb+") as html_file:
+    # with open("page_raw_html.html", "rb+") as html_file:
 
-        html_file.write(page_html.encode("utf-8"))
+    #     html_file.write(page_html.encode("utf-8"))
 
     # print(html.find(".widget.product_short_description", first=True).text)
 
-    # for a in html.find(".widget.product_short_description"):
+    prod_names = []
+    prod_prices = []
+    reg_or_disc = []
+    in_stock = []
 
-    # print(a.text)
+    for prod in html.find(".widget.product_preview.search"):
+        for a in prod.find(".widget.product_short_description"):
+            prod_names.append(a.text)
 
-    for e in html.find(".discount_price"):
-        print(e.text)
+        for b in prod.find(".discount_price"):
+            prod_prices.append(b.text)
+            reg_or_disc.append("Regular Price")
 
-    for f in html.find(".regular_price"):
-        print(f.text)
+        for c in prod.find(".regular_price"):
+            prod_prices.append(c.text)
+            reg_or_disc.append("Discounted Price")
 
-    return
+        for d in prod.find(".widget.ProductPreviewNotPurchasableCustomMessage"):
+            stock = True
+            for e in d.find(".contactforshipping"):
+                stock = False
+            in_stock.append(stock)
+
+    return in_stock[prod_names.index("AmStaff TT3102 Commercial Olympic/Standard Tree")]
 
 
 def send_mail(user, pw):
@@ -42,9 +55,9 @@ def send_mail(user, pw):
 
     server.login(user, pw)
 
-    to = user
-    subject = "Email Subject Line"
-    body = "Email body"
+    to = f"{user};cierrabutty@gmail.com"
+    subject = "Plate Tree Stock"
+    body = f"The plate tree you want is in stock! \n\nLink: https://fitnessavenue.ca/amstaff-tt3102-commercial-olympic-standard-tree"
 
     msg = f"Subject: {subject}\n\n{body}"
 
@@ -62,9 +75,8 @@ if __name__ == "__main__":
     user = "matthew.bagin@gmail.com"
     pw = Hash.decrypt(key, cipher_text).decode("utf-8")
 
-    scrape_page("https://fitnessavenue.ca/zsr3/plate-bar-trees")
-
     # print(f"user name is {user}")
     # print(f"password is {pw}")
 
-    # send_mail(user, pw)
+    if scrape_page("https://fitnessavenue.ca/zsr3/plate-bar-trees"):
+        send_mail(user, pw)
